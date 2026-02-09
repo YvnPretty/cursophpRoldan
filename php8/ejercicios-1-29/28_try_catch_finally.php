b34ebc10c5a43e42a123747dbc8bde0bc4814d3a
Prettyvatt00 <yvngmolly00@gmail.com>
2026-02-09 02:09:33 -0600

    Actualizar 28_try_catch_finally.php

--- php8/ejercicios-1-29/28_try_catch_finally.php
+++ php8/ejercicios-1-29/28_try_catch_finally.php
@@ -1,8 +1,22 @@
 <?php
-// Excepciones (Try Catch)
+// Excepciones (Try Catch) es una extructura de control de errores
+//peromiete ejecutar codigo que puede fallar no es abrupto., finally optional  
 try {
     throw new Exception("Error provocado");
 } catch (Exception $e) {
     echo "Capturado: " . $e->getMessage();
 }
+
+
+try {
+$a = 10;
+$b = 0;
+if($b=== 0){
+}throw new Exception("no se puede divir entre cero");
+}
+
+
+
+
+
 ?>
