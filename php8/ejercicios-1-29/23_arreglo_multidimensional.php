c799de06b9053b06d936b708376aca39abe223dc
Prettyvatt00 <yvngmolly00@gmail.com>
2026-02-09 02:20:29 -0600

    Actualizar 23_arreglo_multidimensional.php

--- php8/ejercicios-1-29/23_arreglo_multidimensional.php
+++ php8/ejercicios-1-29/23_arreglo_multidimensional.php
@@ -1,5 +1,5 @@
 <?php
-// Arreglos Multidimensionales
+// Arreglos Multidimensionales es un arreglo que contiene otros arreglos como datos
 $matriz = [
     [1, 2, 3],
     [4, 5, 6],
@@ -14,9 +14,23 @@ echo $estudiantes[2][0];
 echo $estudiantes[3][0];
 echo $estudiantes[4][0];
 
+$estudiantes = [
+    1 => [
+        "nombre" => "Martín Garrix",
+        "profesion" => "Veterinaria"
+    ],
+    2 => [
+        "nombre" => "Amanda",
+        "profesion" => "Veterinaria"
+    ]
+];
 
+echo "Estudiante número 1<br>";
+echo "Nombre: " . $estudiantes[1]["nombre"] . "<br>";
+echo "Profesión: " . $estudiantes[1]["profesion"] . "<br><br>";
 
-
-
+echo "Estudiante número 2<br>";
+echo "Nombre: " . $estudiantes[2]["nombre"] . "<br>";
+echo "Profesión: " . $estudiantes[2]["profesion"] . "<br>";
 
 ?>
